b4b9c1cb0ebdc537b8a0be7be5c4021c21401136
ldebello <luis.debello@mulesoft.com>
2015-06-13 14:08:51 -0300

    Cleaning unsed imports

--- Sockets/src/main/java/ar/com/javacuriosities/networking/tcp/Step1ServerSocketTCP.java
+++ Sockets/src/main/java/ar/com/javacuriosities/networking/tcp/Step1ServerSocketTCP.java
@@ -4,7 +4,6 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetAddress;
-import java.net.NetworkInterface;
 import java.net.ServerSocket;
 import java.net.Socket;
 
--- Threads/src/main/java/ar/com/javacuriosities/threads/Step12InterruptMethod.java
+++ Threads/src/main/java/ar/com/javacuriosities/threads/Step12InterruptMethod.java
@@ -1,7 +1,5 @@
 package ar.com.javacuriosities.threads;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class Step12InterruptMethod {
 
