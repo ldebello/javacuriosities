294606d7b345e183f2d3917ac774bbb82a278141
ldebello <luis.debello@mulesoft.com>
2015-06-27 09:51:19 -0300

    Adding one example about Time Unit in concurrency material

---
+++ ConcurrencyAPI/src/main/java/ar/com/javacuriosities/concurrency/time_unit/Main.java
@@ -0,0 +1,32 @@
+package ar.com.javacuriosities.concurrency.time_unit;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+import java.util.concurrent.TimeUnit;
+
+/*
+ * TimeUnit: Es una abstracción de las medidas de tiempo, nos permite usar sleep con 
+ * una medida de tiempo o hacer conversiones entre las distintas unidades de tiempo
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            System.out.println("Sleeping 2 seconds...");
+            
+            // Hacemos un sleep por el Time Unit que estemos usando
+            SECONDS.sleep(2);
+            
+            System.out.println("Continue");
+            
+            // Obtenemos una unidad de tiempo
+            TimeUnit timeUnit = TimeUnit.HOURS;
+            
+            // Calculamos la cantidad de horas incluidas en 5 días
+            System.out.println("5 days are: " + timeUnit.convert(5, TimeUnit.DAYS) + " hours");
+        } catch (InterruptedException e) {
+        	// Log and Handle exception
+        	e.printStackTrace();
+        }
+    }
+}
