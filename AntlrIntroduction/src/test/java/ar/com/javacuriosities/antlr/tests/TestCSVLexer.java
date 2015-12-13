3de2e995fbe2f448e77e65e0054e0261064dbf7f
Luis de Bello <luis.debello@mulesoft.com>
2015-12-13 19:34:51 -0300

    Improving lexer test case

--- AntlrIntroduction/src/test/java/ar/com/javacuriosities/antlr/tests/TestCSVLexer.java
+++ AntlrIntroduction/src/test/java/ar/com/javacuriosities/antlr/tests/TestCSVLexer.java
@@ -1,5 +1,7 @@
 package ar.com.javacuriosities.antlr.tests;
 
+import java.util.Scanner;
+
 import org.antlr.runtime.ANTLRStringStream;
 import org.antlr.runtime.Token;
 
@@ -8,16 +10,16 @@ import junit.framework.TestCase;
 
 public class TestCSVLexer extends TestCase {
 	public void testTokens() {
-		
-		String source = "";
-		
-		CSVLexer lexer = new CSVLexer(new ANTLRStringStream(source));
-
-		Token token = lexer.nextToken();
-		
-		while (Token.EOF != token.getType()) {
-			System.out.println(token);
-			token = lexer.nextToken();
+		try (Scanner scanner = new Scanner(Thread.currentThread().getContextClassLoader().getResourceAsStream("Data.csv"))) {
+			String source = scanner.useDelimiter("\\A").next();
+			CSVLexer lexer = new CSVLexer(new ANTLRStringStream(source));
+			
+			Token token = lexer.nextToken();
+			
+			while (Token.EOF != token.getType()) {
+				System.out.println(token);
+				token = lexer.nextToken();
+			}
 		}	
 	}
 }
