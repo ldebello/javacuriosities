16ff691ab724ac85d771e8780e39c884e5f39e5d
Luis de Bello <luis.debello@mulesoft.com>
2016-05-17 14:49:28 -0300

    Adding more info for transactions

--- JDBC/src/main/java/ar/com/javacuriosities/jdbc/Lesson06Transactions.java
+++ JDBC/src/main/java/ar/com/javacuriosities/jdbc/Lesson06Transactions.java
@@ -19,6 +19,19 @@ import java.sql.Statement;
  * rollback: En caso de error podemos usar esto para dejar la base de datos en un estado consistente
  * commit: Nos permite confirmar el set de acciones que ejecutamos y que sean aplicados en la base
  * 
+ * Otra cosa a tener en cuenta con las transacciones es el "Isolation Level"
+ * 
+ * Isolation Level				| Transactions	| Dirty Reads	| Non-Repeatable Reads	| Phantom Reads
+ * TRANSACTION_NONE				| Not supported	| Not applicable| Not applicable		| Not applicable
+ * TRANSACTION_READ_COMMITTED	| Supported		| Prevented		| Allowed				| Allowed
+ * TRANSACTION_READ_UNCOMMITTED	| Supported		| Allowed		| Allowed				| Allowed
+ * TRANSACTION_REPEATABLE_READ	| Supported		| Prevented		| Prevented				| Allowed
+ * TRANSACTION_SERIALIZABLE		| Supported		| Prevented		| Prevented				| Prevented
+ * 
+ * Dirty read: Sucede cuando accedemos a un valor actualizado que aun no fue commiteado
+ * Non-repeatable read: Sucede cuando una transacción A obtiene una fila y esta es actualizada por una transacción B, y en la misma transacción A se vuelve a recuperar la fila pero ahora contiene otro valor
+ * Phantom read: Sucede cuando una transacción A obtiene un conjunto de filas que satisfacen una condición, y ahora una transacción B inserta o actualiza filas para que cumplan con la misma condición, si la transacción A vuelve a pedir los datos puede ver las nuevas filas que cumplen con esta condición 
+ * 
  */
 public class Lesson06Transactions {
 	public static void main(String[] args) {
