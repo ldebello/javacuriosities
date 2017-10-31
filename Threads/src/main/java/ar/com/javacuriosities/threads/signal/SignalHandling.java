6c4263588436ec3357037e338658e42aab4b4557
Luis De Bello <luis.debello@mulesoft.com>
2017-10-31 11:22:18 -0300

    Adding example of signal handling

---
+++ Threads/src/main/java/ar/com/javacuriosities/threads/signal/SignalHandling.java
@@ -0,0 +1,34 @@
+package ar.com.javacuriosities.threads.signal;
+
+
+import sun.misc.Signal;
+import sun.misc.SignalHandler;
+
+import java.util.concurrent.TimeUnit;
+
+public class SignalHandling {
+    public static void main(String[] args) {
+        System.out.println("Program Start");
+        Kill5Handler kill3Handler = new Kill5Handler();
+        Signal.handle(new Signal("TRAP"), kill3Handler);
+
+        try {
+            Thread.sleep(TimeUnit.MINUTES.toMillis(1));
+        } catch (InterruptedException e) {
+            // Log and Handle exception
+            e.printStackTrace();
+        }
+
+        System.out.println("Program End");
+    }
+}
+
+class Kill5Handler implements SignalHandler {
+    public Kill5Handler() {
+    }
+
+    @Override
+    public void handle(Signal signal) {
+        System.out.println("Signal Handling");
+    }
+}
