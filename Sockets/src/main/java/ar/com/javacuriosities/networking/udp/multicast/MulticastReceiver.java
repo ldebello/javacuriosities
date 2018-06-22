790e294c64aaed24c5e078dc053f98bd36873241
Luis De Bello <luis.debello@mulesoft.com>
2018-06-22 12:13:31 -0300

    Mejorando explicacion en caso de error para utilizarIPv4

--- Sockets/src/main/java/ar/com/javacuriosities/networking/udp/multicast/MulticastReceiver.java
+++ Sockets/src/main/java/ar/com/javacuriosities/networking/udp/multicast/MulticastReceiver.java
@@ -8,58 +8,62 @@ import java.net.MulticastSocket;
 /*
  * Existen un rango de IP que son las pertenecientes a las IP clase D, estas se encuentran
  * en el rango 224.0.0.0 - 239.255.255.255.
- * Si el emisor envía un datagrama a esta dirección, se harán multiples copias para todos los interesados
+ * Si el emisor envía un datagrama a esta dirección, se harán multiples copias para todos los interesados.
+ * Al correr este ejemplo puede ser que obtengamos una exception del tipo "java.net.SocketException: Can't assign requested address",
+ * esto nos indica que la dirección solicitada no puede ser asignada un posible motivo para eso es que NetworkInterface.getDefault esta
+ * tomando una NIC incorrecta (En este caso seria una NIC solo de IPv6). Esto lo debemos solucionar usando -Djava.net.preferIPv4Stack=true.
+ *
  */
 public class MulticastReceiver {
 
-	public static volatile boolean isRunning = true;
+    public static volatile boolean isRunning = true;
 
-	public static void main(String[] args) {
-		MulticastSocket multicastSocket = null;
+    public static void main(String[] args) {
+        MulticastSocket multicastSocket = null;
 
-		InetAddress group1 = null;
-		InetAddress group2 = null;
-		try {
-			// Creamos un socket multicast en el puerto 10000:
-			multicastSocket = new MulticastSocket(10000);
-			
-			// Configuramos el grupo (IP) a la que nos conectaremos:
-			group1 = InetAddress.getByName("231.0.0.1");
-			group2 = InetAddress.getByName("231.0.0.2");
+        InetAddress group1 = null;
+        InetAddress group2 = null;
+        try {
+            // Creamos un socket multicast en el puerto 10000:
+            multicastSocket = new MulticastSocket(10000);
 
-			// Nos unimos al grupo: (No podemos unirnos dos veces al mismo grupo)
-			multicastSocket.joinGroup(group1);
-			multicastSocket.joinGroup(group2);
+            // Configuramos el grupo (IP) a la que nos conectaremos:
+            group1 = InetAddress.getByName("231.0.0.1");
+            group2 = InetAddress.getByName("231.0.0.2");
 
-			while (isRunning) {
-				// Recibimos el paquete del socket
-				byte[] buffer = new byte[100];
+            // Nos unimos al grupo: (No podemos unirnos dos veces al mismo grupo)
+            multicastSocket.joinGroup(group1);
+            multicastSocket.joinGroup(group2);
 
-				DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+            while (isRunning) {
+                // Recibimos el paquete del socket
+                byte[] buffer = new byte[100];
 
-				multicastSocket.receive(packet);
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
 
-				System.out.println(new String(packet.getData(), 0, packet.getLength()));
+                multicastSocket.receive(packet);
 
-				System.out.println("Remote Port:" + packet.getPort());
-				System.out.println("Remote Address:" + packet.getAddress());
-				System.out.println("Remote Socket Address:" + packet.getSocketAddress());
-			}
-		} catch (IOException e) {
-			// Log and Handle exception
-			e.printStackTrace();
-		} finally {
-			// Salimos de los grupo multicast
-			try {
-				multicastSocket.leaveGroup(group1);
-				multicastSocket.leaveGroup(group2);
-			} catch (IOException e) {
-				// Log and Handle exception
-				e.printStackTrace();
-			}
+                System.out.println(new String(packet.getData(), 0, packet.getLength()));
 
-			// Cerramos el socket:
-			multicastSocket.close();
-		}
-	}
+                System.out.println("Remote Port:" + packet.getPort());
+                System.out.println("Remote Address:" + packet.getAddress());
+                System.out.println("Remote Socket Address:" + packet.getSocketAddress());
+            }
+        } catch (IOException e) {
+            // Log and Handle exception
+            e.printStackTrace();
+        } finally {
+            // Salimos de los grupo multicast
+            try {
+                multicastSocket.leaveGroup(group1);
+                multicastSocket.leaveGroup(group2);
+            } catch (IOException e) {
+                // Log and Handle exception
+                e.printStackTrace();
+            }
+
+            // Cerramos el socket:
+            multicastSocket.close();
+        }
+    }
 }
