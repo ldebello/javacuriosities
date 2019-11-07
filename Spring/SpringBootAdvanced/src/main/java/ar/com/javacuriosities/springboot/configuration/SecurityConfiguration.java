f224e676883b9aa4d68f4957d97793e5d68b1cad
Luis De Bello <luis.debello@mulesoft.com>
2019-11-07 10:16:26 -0300

    Fixing compilation issue for springboot 2.2.0

--- Spring/SpringBootAdvanced/src/main/java/ar/com/javacuriosities/springboot/configuration/SecurityConfiguration.java
+++ Spring/SpringBootAdvanced/src/main/java/ar/com/javacuriosities/springboot/configuration/SecurityConfiguration.java
@@ -61,7 +61,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    public void configure(WebSecurity web) throws Exception {
+    public void configure(WebSecurity web) {
         web
             .ignoring()
             .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
