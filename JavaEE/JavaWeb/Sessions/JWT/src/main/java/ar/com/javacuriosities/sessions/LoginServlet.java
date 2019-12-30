73d47d5139c339c2d7f9ecdc707cdef1d8c9ca7f
Luis De Bello <luis.debello@mulesoft.com>
2019-12-29 23:34:57 -0300

    Adding example of JWT session

--- JavaEE/JavaWeb/Sessions/JWT/src/main/java/ar/com/javacuriosities/sessions/LoginServlet.java
+++ JavaEE/JavaWeb/Sessions/JWT/src/main/java/ar/com/javacuriosities/sessions/LoginServlet.java
@@ -1,14 +1,16 @@
 package ar.com.javacuriosities.sessions;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-public class WelcomeServlet extends HttpServlet {
+public class LoginServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -22,27 +24,32 @@ public class WelcomeServlet extends HttpServlet {
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html;charset=UTF-8");
+
         try (PrintWriter out = response.getWriter()) {
-            Cookie[] cookies = request.getCookies();
+            // Recuperamos el nombre de usuario
+            String user = request.getParameter("txtUsuario");
+            String jwt = createSignedJWT(user, "manager");
+
+            Cookie cookie = new Cookie("JWT-SESSION-ID", jwt);
+
+            response.addCookie(cookie);
 
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Welcome</title>");
+            out.println("<title>Login</title>");
             out.println("</head>");
             out.println("<body>");
-            if (cookies != null) {
-                out.println("<h2>Cookies</h2>");
-                for (int i = 0; i < cookies.length; i++) {
-                    Cookie cookie = cookies[i];
-                    out.print("<b>Name: </b>" + cookie.getName() + " ");
-                    out.print("<b>Value: </b>" + cookie.getValue() + " <br/>");
-                }
-            } else {
-                out.println("<h2>No cookies found</h2>");
-            }
+            out.println("Bienvenido " + user);
+            out.println("<form action='WelcomeServlet'>");
+            out.println("<input type='submit' value='JWT'>");
+            out.println("</form>");
             out.println("</body>");
             out.println("</html>");
         }
+    }
+
+    private String createSignedJWT(String user, String role) {
+        return JWT.create().withClaim("user", user).withClaim("role", role).sign(Algorithm.HMAC256("secret"));
     }
 }
--- JavaEE/JavaWeb/Sessions/JWT/src/main/java/ar/com/javacuriosities/sessions/WelcomeServlet.java
+++ JavaEE/JavaWeb/Sessions/JWT/src/main/java/ar/com/javacuriosities/sessions/WelcomeServlet.java
@@ -1,10 +1,13 @@
 package ar.com.javacuriosities.sessions;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -23,7 +26,7 @@ public class WelcomeServlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            Cookie[] cookies = request.getCookies();
+            Cookie cookie = getCookieFromRequest(request, "JWT-SESSION-ID");
 
             out.println("<!DOCTYPE html>");
             out.println("<html>");
@@ -31,18 +34,35 @@ public class WelcomeServlet extends HttpServlet {
             out.println("<title>Welcome</title>");
             out.println("</head>");
             out.println("<body>");
-            if (cookies != null) {
-                out.println("<h2>Cookies</h2>");
-                for (int i = 0; i < cookies.length; i++) {
-                    Cookie cookie = cookies[i];
-                    out.print("<b>Name: </b>" + cookie.getName() + " ");
-                    out.print("<b>Value: </b>" + cookie.getValue() + " <br/>");
-                }
+            if (cookie != null) {
+                String jwt = cookie.getValue();
+                JWTVerifier verifier = JWT.require(Algorithm.HMAC256("secret")).build();
+
+                verifier.verify(jwt).getClaims().forEach((key, value) -> {
+                    out.print("<b>Name: </b>" + key + " ");
+                    out.print("<b>Value: </b>" + value.asString() + " <br/>");
+                });
             } else {
-                out.println("<h2>No cookies found</h2>");
+                out.println("<h2>No token found</h2>");
             }
             out.println("</body>");
             out.println("</html>");
         }
+    }
+
+    private Cookie getCookieFromRequest(HttpServletRequest request, String name) {
+        Cookie cookie = null;
+        if (request != null) {
+            Cookie[] cookies = request.getCookies();
+            if (cookies != null) {
+                for (int i = 0; i < cookies.length; i++) {
+                    Cookie currentCookie = cookies[i];
+                    if (currentCookie != null && currentCookie.getName().equals(name)) {
+                        cookie = currentCookie;
+                    }
+                }
+            }
+        }
+        return cookie;
     }
 }
