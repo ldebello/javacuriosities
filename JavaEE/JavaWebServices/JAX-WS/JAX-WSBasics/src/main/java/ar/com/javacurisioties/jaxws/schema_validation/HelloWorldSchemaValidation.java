382edeea76a105eb3173acb494e18180563b50e8
Luis De Bello <luis.debello@mulesoft.com>
2020-02-06 00:07:10 -0300

    Adding jax-ws dependencies

--- JavaEE/JavaWebServices/JAX-WS/JAX-WSBasics/src/main/java/ar/com/javacurisioties/jaxws/schema_validation/HelloWorldSchemaValidation.java
+++ JavaEE/JavaWebServices/JAX-WS/JAX-WSBasics/src/main/java/ar/com/javacurisioties/jaxws/schema_validation/HelloWorldSchemaValidation.java
@@ -1,6 +1,6 @@
 package ar.com.javacurisioties.jaxws.schema_validation;
 
-import com.sun.xml.internal.ws.developer.SchemaValidation;
+import com.sun.xml.ws.developer.SchemaValidation;
 
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
@@ -12,7 +12,7 @@ import javax.xml.ws.RequestWrapper;
 public class HelloWorldSchemaValidation {
 
     @WebMethod(operationName = "simpleQuery")
-    public void without_wrapper(Parameters parameters) {
+    public void withoutWrapper(Parameters parameters) {
         System.out.println("Code: " + parameters.getCode());
         System.out.println("Start: " + parameters.getStart());
         System.out.println("End: " + parameters.getEnd());
