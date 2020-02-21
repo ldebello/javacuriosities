dc27b35cf9ad5a37d443033095cb95e3c0fa859c
Luis De Bello <luis.debello@mulesoft.com>
2020-02-21 00:23:13 -0300

    Fixing header location

--- Spring/SpringBootBasics/src/main/java/ar/com/javacuriosities/springboot/controller/TaskController.java
+++ Spring/SpringBootBasics/src/main/java/ar/com/javacuriosities/springboot/controller/TaskController.java
@@ -5,18 +5,15 @@ import ar.com.javacuriosities.springboot.model.Task;
 import ar.com.javacuriosities.springboot.model.request.TaskRequest;
 import ar.com.javacuriosities.springboot.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.net.URISyntaxException;
-
-import static java.lang.String.format;
 
 @RestController
 @RequestMapping("/tasks")
@@ -43,16 +40,14 @@ public class TaskController {
 
     @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity save(@RequestBody TaskRequest taskRequest) {
-        try {
-            String description = taskRequest.getDescription();
-            if (description != null && description.contains("dropwizard")) {
-                throw new InvalidDescriptionException("Description does not make any sense");
-            }
-            Task newTask = taskService.save(taskRequest);
-            URI location = new URI(format(HttpHeaders.LOCATION, newTask.getId()));
-            return ResponseEntity.created(location).body(newTask);
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
+        String description = taskRequest.getDescription();
+        if (description != null && description.contains("dropwizard")) {
+            throw new InvalidDescriptionException("Description does not make any sense");
         }
+        Task newTask = taskService.save(taskRequest);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("/{id}")
+                .buildAndExpand(newTask.getId()).toUri();
+        return ResponseEntity.created(location).body(newTask);
     }
 }
