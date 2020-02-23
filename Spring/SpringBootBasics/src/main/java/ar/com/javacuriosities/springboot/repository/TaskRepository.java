178deffbd906227808320a5ea937ec9b0c09cf4f
Luis De Bello <luis.debello@mulesoft.com>
2020-02-23 08:12:16 -0300

    Changing to use PagingAndSortingRepository

--- Spring/SpringBootBasics/src/main/java/ar/com/javacuriosities/springboot/repository/TaskRepository.java
+++ Spring/SpringBootBasics/src/main/java/ar/com/javacuriosities/springboot/repository/TaskRepository.java
@@ -1,15 +1,11 @@
 package ar.com.javacuriosities.springboot.repository;
 
 import ar.com.javacuriosities.springboot.model.Task;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
 
-public interface TaskRepository extends CrudRepository<Task, Long> {
-
-    Page<Task> findAll(Pageable pageable);
+public interface TaskRepository extends PagingAndSortingRepository<Task, Long> {
 
     List<Task> findByDescription(String description);
 }
